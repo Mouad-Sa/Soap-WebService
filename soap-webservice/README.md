4c95c3f474e70333fd26cd743aee29b59238886f
Mouad Sattani <mo_sattani@etu.enset-media.ac.ma>
2025-02-05 10:07:58 +0100

    Add files via upload

---
+++ soap-webservice/README.md
@@ -0,0 +1,14 @@
+# Currency-converter-soap-webservice
+
+<img src="./READMEIMGS/generated_WSDL.png">
+<br>
+<h2>webService Tests Soap UI :</h2>
+<br>
+<h4>Euro_to_DH_converter :</h4>
+<img src="./READMEIMGS/converter.png">
+<br>
+<h4>getAccount :</h4>
+<img src="./READMEIMGS/getAccount.png">
+<br>
+<h4>getAccountList :</h4>
+<img src="./READMEIMGS/accountList.png">
---
+++ soap-webservice/server/src/main/java/ws/BankService.java
@@ -0,0 +1,31 @@
+package ws;
+
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+import jakarta.jws.WebService;
+
+import java.util.Date;
+import java.util.List;
+@WebService(serviceName="BankConverterWS")
+public class BankService {
+    public BankService() {}
+
+    @WebMethod(operationName = "Euro_to_DH_converter")
+    public double converstion(@WebParam double amount) {
+        return amount * 11;
+    }
+
+    @WebMethod
+    public Account getAccount(@WebParam int code) {
+        return new Account(code,new Date(),Math.random()*10000);
+    }
+
+    @WebMethod
+    public List<Account> getAccountList(){
+        return  List.of(
+                new Account(1,new Date(),Math.random()*10000),
+                new Account(2,new Date(),Math.random()*10000),
+                new Account(3,new Date(),Math.random()*10000)
+        );
+    }
+}
